0a2272cf975faf5b4e45f71d40b12fa89723ac83
Aleeza-Maryam <aleezamarryam563@gmail.com>
2026-03-10 19:35:10 +0500

    Added new files

--- numpy_random_func.py
+++ numpy_random_func.py
@@ -12,3 +12,16 @@ randn=np.random.randn(4)
 print(randn)
 randn_2d=np.random.randn(2,3)
 print(randn_2d)
+
+
+# ranf()
+ranf=np.random.ranf(4)  # random float  1d array with 4 random values between 0.0(include) and 1.0(exclude)
+print(ranf)
+ranf_2d=np.random.ranf((2,3))
+print(ranf_2d)
+
+# randint()     ye 2 numbers k beech random value generate krta
+randint=np.random.randint(1,10,5 )  #1 to 10 ke beech 5 random integers generate krta hai
+print(randint)
+randint_2d=np.random.randint(1,20,(2,3))
+print(randint_2d)
