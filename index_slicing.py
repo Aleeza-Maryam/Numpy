3909da374182b938d8115d9229f4b6f233c43277
Aleeza-Maryam <aleezamarryam563@gmail.com>
2026-03-17 12:47:25 +0500

    1d slicing

--- index_slicing.py
+++ index_slicing.py
@@ -29,4 +29,17 @@ print(var3)
 
 print(var3[1,1,1])
 print(var3[0])   #pora 1st matrix
-print(var3[0,1,])
+print(var3[0,1,])     #comma lgao end pe ya na lgao
+
+
+#------------------------------------------------- slicing-------------------------------------------------------
+# 1d array
+sl1=np.array([1,2,3,4])
+print(sl1[1:3])
+print(sl1[1:])
+print(sl1[:])
+
+# step / jump
+sl2=np.array([1,2,3,4,5,6,7,8,9,10])
+print(sl2[::2])          #1 word ki jump
+print(sl2[2:8:1])
