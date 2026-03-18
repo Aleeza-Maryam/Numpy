8976a173387a5b288bed6ae388808d1832fc9d5d
Aleeza-Maryam <aleezamarryam563@gmail.com>
2026-03-18 19:30:58 +0500

    neditor and ndenumerate

--- interation_neditor.py
+++ interation_neditor.py
@@ -34,3 +34,50 @@ for i in var3:
     for j in i:
         for k in j:
             print(k)
+
+
+
+# neditor function agr baar baar for loop ni lgana chahte
+print("nditer")
+var4=np.array([
+    [
+        [1,2,3,4],
+        [5,6,7,8]
+    ],
+    [
+        [7,6,5,4],
+        [9,8,7,6]
+    ]
+])
+for i in np.nditer(var4):
+    print(i)
+
+var5=np.array([
+    [1,2,3,4],
+    [5,6,7,8]
+])
+print("var5")
+for i in np.nditer(var5):
+    print(i)
+
+# parameters
+for i in np.nditer(var5,flags=['buffered'], op_dtypes="S"):    #buffered wle flag mai iterate wla data store hoga
+    print(i)
+
+# denumerate function se hum inde bhi sath print kr skte
+for d,i in np.ndenumerate(var5):
+    print(d,i)
+
+
+var6=np.array([
+    [
+        [1,2,3,4],
+        [5,6,7,8]
+    ],
+    [
+        [7,6,5,4],
+        [9,8,7,6]
+    ]
+])
+for i,d in np.ndenumerate(var6):
+    print(i,d)
