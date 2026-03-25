a14aa8bd8539e844bf37b64b2c4756c2ef03b406
Aleeza-Maryam <aleezamarryam563@gmail.com>
2026-03-25 14:03:43 +0500

    wine quality project level 2

--- wine_quality.py
+++ wine_quality.py
@@ -26,3 +26,40 @@ print("standard deviation of each row ",np.std(data,axis=0))
 # poore table ka std
 print("std of full table ",np.std(data))
 print("mean of full table ",np.mean(data))
+
+
+
+
+# Level 2: Slicing & Filtering
+# Sirf "Quality" wala column (akhri column) extract karein.
+
+# Un tamam wines ka data alag karein jinki quality 7 ya usse zyada hai.
+
+# Column number 3 (Citric Acid) ki values ko extract kar ke ek naya array banayein.
+
+# "Fixed Acidity" (1st column) ki 10th se 20th row tak ka data dikhayein.
+
+# Woh rows filter karein jahan "Alcohol" ki percentage 10 se kam ho.
+
+
+quality_col=data[:,-1]
+print(quality_col)
+greater_7=quality_col>7
+var=data[greater_7]
+print("Alcohol quality greate or equal 7 ",var)
+print("count ",len(var))
+
+citric=data[:,2]
+print("citric acid col ",citric)
+
+
+# "Fixed Acidity" (1st column) ki 10th se 20th row tak ka data dikhayein.
+fixed_Acidity=data[10:21,0]
+print("fixed acidity rw 10 se 20 ",fixed_Acidity)
+
+
+# Woh rows filter karein jahan "Alcohol" ki percentage 10 se kam ho.
+alcohol=data[:,-2]
+alcohol_filter=data[alcohol<10]
+print("percentage less than 10",alcohol_filter)
+print("count ",len(alcohol_filter))
