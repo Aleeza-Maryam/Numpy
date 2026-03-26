d6882c39c15775dcc87661079d943ad41fa207f7
Aleeza-Maryam <aleezamarryam563@gmail.com>
2026-03-26 14:34:24 +0500

    level3

--- wine_quality.py
+++ wine_quality.py
@@ -63,3 +63,67 @@ alcohol=data[:,-2]
 alcohol_filter=data[alcohol<10]
 print("percentage less than 10",alcohol_filter)
 print("count ",len(alcohol_filter))
+
+
+# Level 3: Data Manipulation & Logic
+# Pura dataset check karein ke kahin koi NaN (Null) value toh nahi.
+
+# "PH" column ki saari values ko update karein (e.g., har value mein 0.1 add karein).
+
+# "Quality" column mein check karein ke kitni unique values hain.
+
+# Ek naya column (array) banayein jo "Fixed Acidity" aur "Volatile Acidity" ka sum ho.
+
+# Dataset ko "Alcohol" column ke mutabiq sort karein (ascending order).
+
+nan=np.isnan(data).any()
+print(nan)
+nansum=sum(np.isnan(data))
+print(nansum)
+
+
+# "PH" column ki saari values ko update karein (e.g., har value mein 0.1 add karein).
+phsum=data[:,-4]+0.1
+print(phsum)
+
+# "Quality" column mein check karein ke kitni unique values hain.
+qual_unique=np.unique(data[:,-1])
+print(qual_unique)
+
+# Ek naya column (array) banayein jo "Fixed Acidity" aur "Volatile Acidity" ka sum ho.
+
+sum_fix_vol=data[:,0]+data[:,1]
+print(sum_fix_vol)
+
+# np.column_stack do arrays ko side-by-side jor deta hai
+# Humein sum_fix_vol ko 2D banana parega iske liye
+new_dataset = np.column_stack((data, sum_fix_vol))
+
+print("Naya dataset shape:", new_dataset.shape)
+
+np.savetxt('new_wine_data.csv', new_dataset, delimiter=',')
+
+print("new excel file saved.")
+
+
+# Dataset ko "Alcohol" column ke mutabiq sort karein (ascending order).
+alcohol_asc=data[:,-2]
+sorted_array=np.sort(alcohol_asc)
+print(sorted_array)
+
+# poore datet ko alcohol k according sort
+sort_indice=np.argsort(data[:,10])
+sorted_dataset=data[sort_indice]
+print("sorted cols ",sorted_dataset[:,:])
+
+# 1. Pehle normal argsort karein (Ascending)
+sort_indices = np.argsort(data[:, 10])
+
+# 2. Is index array ko reverse kar dein [::-1] use karke
+descending_indices = sort_indices[::-1]
+
+# 3. Ab data ko in naye indexes ke mutabiq arrange karein
+sorted_descending = data[descending_indices]
+
+# Check karein: Pehli 5 rows ka alcohol (Index 10) - Ab ye sabse zyada honi chahiye
+print("Sorted Alcohol (Highest to Lowest):\n", sorted_descending[:5, 10])
